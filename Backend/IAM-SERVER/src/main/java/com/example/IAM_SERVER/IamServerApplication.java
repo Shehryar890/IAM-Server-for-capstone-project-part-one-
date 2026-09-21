f75ff44f18e2f4567eec44236af8e1b0435bef75
Shehryar890 <sherymalik17786@gmail.com>
2026-09-21 12:38:41 +0500

    IAM-SERVER first commit on main

---
+++ Backend/IAM-SERVER/src/main/java/com/example/IAM_SERVER/IamServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.IAM_SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamServerApplication.class, args);
+	}
+
+}
---
+++ Backend/IAM-SERVER/src/test/java/com/example/IAM_SERVER/IamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IAM_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
